1d1d1ce68cfaa3d4c0c9e2fdbafe961d427ee0e2
vlad gurtovoy <tony_iommi@mail.ru>
2017-11-28 13:10:30 +0300

    Добавил создание экрана в главном классе игры

--- core/src/snakes/game/MyGdxGame.java
+++ core/src/snakes/game/MyGdxGame.java
@@ -2,11 +2,13 @@ package snakes.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import snakes.screens.GameScreen;
 
 public class MyGdxGame extends Game {
     
     @Override
     public void create() {
-        Gdx.app.log("ZBGame", "created");
+        Gdx.app.log("Snakes", "created");
+        setScreen(new GameScreen());
     }
 }
