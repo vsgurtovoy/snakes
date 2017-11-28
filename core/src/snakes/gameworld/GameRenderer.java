c44ccc0176c2bf0a56c8ab7ad7be8e8b942507c5
vlad gurtovoy <tony_iommi@mail.ru>
2017-11-28 13:44:30 +0300

    Добавлена камера и шейдер для отрисовки

--- core/src/snakes/gameworld/GameRenderer.java
+++ core/src/snakes/gameworld/GameRenderer.java
@@ -1,12 +1,20 @@
 package snakes.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GameRenderer {
     private GameWorld myWorld;
+    private OrthographicCamera cam;
+    private ShapeRenderer shapeRenderer;
     
     public GameRenderer(GameWorld world) {
         myWorld = world;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, 204);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(cam.combined);
     }
     
     public void render() {
