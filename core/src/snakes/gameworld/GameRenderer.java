16f6d0b5767ecd33eeb3f1adc437837723cd6726
vlad gurtovoy <tony_iommi@mail.ru>
2017-11-28 13:50:45 +0300

    Добавил тестовую отрисовку прямоугольника

--- core/src/snakes/gameworld/GameRenderer.java
+++ core/src/snakes/gameworld/GameRenderer.java
@@ -1,8 +1,10 @@
 package snakes.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class GameRenderer {
     private GameWorld myWorld;
@@ -19,5 +21,46 @@ public class GameRenderer {
     
     public void render() {
         Gdx.app.log("GameRenderer", "render");
+
+        /*
+         * 1. Мы рисуем черный задний фон, чтобы избавится от моргания и следов от передвигающихся объектов
+         */
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        /*
+         * 2. Мы отрисовываем однотонный квадрат
+         */
+
+        // Говорим shapeRenderer начинать отрисовывать формы
+        shapeRenderer.begin(ShapeType.Filled);
+
+        // Выбираем RGB Color 87, 109, 120, не прозрачный
+        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Отрисовываем квадрат из myWorld (Используем ShapeType.Filled)
+        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
+                myWorld.getRect().width, myWorld.getRect().height);
+
+        // говорим shapeRenderer прекратить отрисовку
+        // Мы ДОЛЖНЫ каждый раз это делать
+        shapeRenderer.end();
+
+        /*
+         * 3. Мы отрисовываем рамку для квадрата
+         */
+
+        // Говорим shapeRenderer нарисовать рамку следующей формы
+        shapeRenderer.begin(ShapeType.Line);
+
+        // Выбираем цвет RGB Color 255, 109, 120, не прозрачный
+        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Отрисовываем квадрат из myWorld (Using ShapeType.Line)
+        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
+                myWorld.getRect().width, myWorld.getRect().height);
+
+        shapeRenderer.end();
     }
 }
