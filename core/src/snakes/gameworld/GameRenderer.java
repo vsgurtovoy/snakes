df0ad69556e3647a833e280624adbb4cddc0cb25
vlad gurtovoy <tony_iommi@mail.ru>
2017-11-29 19:24:13 +0300

    Добавил комментарии к коду в классе GameRenderer

--- core/src/snakes/gameworld/GameRenderer.java
+++ core/src/snakes/gameworld/GameRenderer.java
@@ -1,14 +1,12 @@
 package snakes.gameworld;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import snakes.helpers.AssetLoader;
 import snakes.objects.Apple;
 import snakes.objects.Battery;
