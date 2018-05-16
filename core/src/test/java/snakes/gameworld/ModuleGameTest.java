c45d8df70264e5064f00ce855fb73d232cab050d
Stanislav Anisimov <s.anisimov23@gmail.com>
2018-05-17 00:00:00 +0300

    Test test

---
+++ core/src/test/java/snakes/gameworld/ModuleGameTest.java
@@ -0,0 +1,12 @@
+package snakes.gameworld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ModuleGameTest {
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+
+}
